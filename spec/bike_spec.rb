7fdba12ea27f31a953d247e69c51546cc23e676a
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-03 16:30:27 +0000

    added docking station tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,7 +4,8 @@ describe Bike do
 	
 	let(:bike){Bike.new} #let = the test creates a Bike.new when it sees it in a statement and let! creates it even if it's not in the statement.
 	let(:broken_bike){Bike.new.break!}
-	
+	let(:station){DockingStation.new}
+
 	it 'should not be broken after we create it' do
 		expect(bike).not_to be_broken
 	end
@@ -15,5 +16,14 @@ describe Bike do
 
 	it 'should be able to be fixed' do
 		expect(broken_bike.fix!).not_to be_broken
+	end
+
+	it 'should release the bike' do
+		station.dock(bike)
+		station.release(bike)
+		expect(station.bike_count).to eq(0)	
 	end	
+
+	
+
 end		
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,18 @@
-describe DockingStation do 
-  it "Should accept a bike" do
-  	bike = Bike.new
-  	station = DockingStation.new
-  	expect(station.bike_count).to eq(0)
-  	station.dock(bike)
-  	expect(station.bike_count).to eq(1)
-  end
+require 'bike'
 
-	
+describe DockingStation do 
+		let(:station){DockingStation.new(capacity: 20)}
+		it "Should accept a bike" do
+			bike = Bike.new
+			station = DockingStation.new
+			expect(station.bike_count).to eq(0)
+			station.dock(bike)
+			expect(station.bike_count).to eq(1)
+		end
+
+		it 'Should know when it\'s full' do
+			expect(station).not_to be_full
+			20.times{station.dock(Bike.new)}
+			expect(station).to be_full
+		end	
 end
