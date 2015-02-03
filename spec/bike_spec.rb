71ddf61426252137988eaa247d8f5ff4c45ebd46
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-03 13:38:08 +0000

    added broken test

---
+++ spec/bike_spec.rb
@@ -0,0 +1,17 @@
+require 'bike'
+#we're describing the functionality of a specific class, Bike
+describe Bike do
+	# this is a specific feature (behaviour)
+	# that we expect to be present
+	it 'should not be broken after we create it' do
+		the_bike = Bike.new # initialise a new object of Bike class
+		# expect an instance of this class to have
+		# a method "broken?" that should return false
+		expect(the_bike).not_to be_broken
+	end
+	it 'should be able to break' do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end		
+end		
