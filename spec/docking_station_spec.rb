8831138ff2177f6fe77fee8df166e20045a11b7a
Chris Ward <chriswarddev@gmail.com>
2015-02-03 16:07:38 +0000

    added full? method and capacity instance variable

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 
 describe DockingStation do
 	let (:bike) {Bike.new}
-	let (:station) {DockingStation.new}
+	let (:station) {DockingStation.new(capacity: 20)}
 	
 	it "should accept a bike" do
 		expect(station.bike_count).to eq(0)
@@ -17,4 +17,10 @@ describe DockingStation do
 		expect(station.bike_count).to eq(0)
 	end
 
+	it "should know when it's full" do
+		expect(station).not_to be_full
+		20.times {station.dock(Bike.new)}
+		expect(station).to be_full
+		
+	end
 end
