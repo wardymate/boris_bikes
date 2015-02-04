785fd1aa08cdccd27c58985d4de375762ea5a988
Chris Ward <chriswarddev@gmail.com>
2015-02-04 16:08:18 +0000

    tests added to the bike container

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -4,11 +4,40 @@ class ContainerHolder; include BikeContainer; end
 
 describe BikeContainer do 
 	let(:bike){Bike.new}
+	let(:broken_bike){Bike.new}
 	let(:holder){ContainerHolder.new}
 	it 'should accept a bike' do 
 		expect(holder.bike_count).to eq(0)
 		holder.dock(bike)
 		expect(holder.bike_count).to eq(1)
 	end	
+
+	it "should release a bike" do
+		holder.dock(bike)
+		holder.release(bike)
+		expect(holder.bike_count).to eq(0)
+
+	end
+	
+	it "should know when it's full" do
+		expect(holder).not_to be_full
+  		10.times {holder.dock(bike) }
+  		expect(holder).to be_full
+	end
+
+	it 'should not accept a bike if it\'s full' do
+  		10.times { holder.dock(bike) }
+  		expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
+	end
+
+	it "should provide the list of available bikes" do
+		broken_bike.break!
+		holder.dock(bike)
+		holder.dock(broken_bike)
+		expect(holder.available_bikes).to eq([bike])
+	end
+
+	
+
 end
 
