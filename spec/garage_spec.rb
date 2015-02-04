a0b9828534cd6cb4bc19b1d8267278d23a60c9f6
Chris Ward <chriswarddev@gmail.com>
2015-02-04 17:18:08 +0000

    replaced class instances with doubles

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,19 +1,23 @@
 require './lib/garage'
 
-class ContainerHolder; include BikeContainer; end
+#class ContainerHolder; include BikeContainer; end
 
 	describe Garage do
 	
 		let(:bike) {Bike.new}
-		let(:holder) {ContainerHolder.new}
+		let(:broken_bike) {Bike.new.break!}
+		let(:garage) {Garage.new}
 
 		it 'should accept the bike' do
-			expect(holder.bike_count).to eq(0)
-			holder.dock(bike)
-			expect(holder.bike_count).to eq(1)
+			expect(garage.bike_count).to eq(0)
+			garage.dock(bike)
+			expect(garage.bike_count).to eq(1)
 		end
 
 		it 'should repair the bike' do
-			expect(bike.fix!).not_to be_broken
+			expect(broken_bike.broken?).to be true
+			garage.accept(broken_bike)
+			expect(bike.broken?).to be false
 		end
+
 	end
