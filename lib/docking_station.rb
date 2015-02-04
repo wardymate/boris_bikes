1c14baec95779d814a79bf1d931686fdf03e8665
Chris Ward <chriswarddev@gmail.com>
2015-02-04 13:18:55 +0000

    created docking station test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,36 +1,13 @@
+require_relative 'bike_container'
+
 class DockingStation
+
+	include BikeContainer
+
 	DEFAULT_CAPACITY = 10
 
 	def initialize(options = {})
 		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@bikes =[]
-	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		raise 'invalid release request' if bike == nil 
-		raise 'No bikes available' if empty?
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject { |bike| bike.broken? }
-	end
-
-	def empty?
-		bike_count == 0
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,59 +1,13 @@
-require 'docking_station'
+require './lib/docking_station'
 
 describe DockingStation do
-	let (:bike)        { double :bike, break!: true, broken?: false }
-	let (:broken_bike) { double :bike, break!: true, broken?: true  }
-	let (:station)     { DockingStation.new(capacity: 20)           }
-
-	def fill_station(station)
-		20.times {station.dock(bike)}
-	end
-
-	def empty_station(station)
-		station.bike_count.times {station.release(bike)}
-	end
 	
-	it "should accept a bike" do
-		expect(station.bike_count).to eq(0)
-		station.dock(bike)
-		expect(station.bike_count).to eq(1)
+	let (:station)     { DockingStation.new(capacity: 123)           }
+
+	it 'should allow setting default capacity on initialising' do 
+		expect(station.capacity).to eq(123)
 	end
-
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		fill_station station
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it's full" do
-		fill_station station
-		expect{ station.dock(bike) }.to raise_error(RuntimeError, 'Station is full')
-	end
-
-	it "should provide the list of available bikes" do
-		broken_bike.break!
-		station.dock(bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([bike])
-	end
-
- 	it "should know when its empty" do
- 		expect(station).to be_empty
- 	end
-
- 	it "should not release a bike that is not there" do
- 		empty_station station
- 		expect{ station.release(bike) }.to raise_error(RuntimeError, 'No bikes available')
- 	end
-
- 	it "should not release a bike if there's no argument passed" do
- 		expect{ station.release(nil) }.to raise_error( 'invalid release request')
- 	end
+		
+	
 
 end
