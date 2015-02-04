dc344fce09ec86e0495fdd8b931b808b69005095
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-02-04 16:44:24 +0000

    refactored bikes testing code

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -6,14 +6,23 @@ describe BikeContainer do
 	let(:bike) { double :bike, break!: true, broken?: false}
 	let(:broken_bike) { double :bike, break!: true, broken?: true }
 	let(:holder){ContainerHolder.new}
+
+	def fill_holder(holder)
+		10.times{holder.dock(bike)}
+	end	
+
+	def dock_bike(bike)
+		holder.dock(bike)
+	end	
+
 	it 'should accept a bike' do 
 		expect(holder.bike_count).to eq(0)
-		holder.dock(bike)
+		dock_bike(bike)
 		expect(holder.bike_count).to eq(1)
 	end	
 
 	it "should release a bike" do
-		holder.dock(bike)
+		dock_bike(bike)
 		holder.release(bike)
 		expect(holder.bike_count).to eq(0)
 
@@ -21,19 +30,19 @@ describe BikeContainer do
 	
 	it "should know when it's full" do
 		expect(holder).not_to be_full
-  		10.times {holder.dock(bike) }
+		fill_holder(holder)
   		expect(holder).to be_full
 	end
 
 	it 'should not accept a bike if it\'s full' do
-  		10.times { holder.dock(bike) }
+  		fill_holder(holder)
   		expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
 	end
 
 	it "should provide the list of available bikes" do
 		broken_bike.break!
-		holder.dock(bike)
-		holder.dock(broken_bike)
+		dock_bike(bike)
+		dock_bike(broken_bike)
 		expect(holder.available_bikes).to eq([bike])
 	end
 
