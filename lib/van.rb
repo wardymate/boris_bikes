b30304f67675cdc47694ab7c50d0ddb1afd7cedf
Chris Ward <chriswarddev@gmail.com>
2015-02-05 14:33:04 +0000

    test for van checking for available bikes

--- lib/van.rb
+++ lib/van.rb
@@ -4,5 +4,10 @@ class Van
 
 	include BikeContainer
 
+	def ask_for_bikes(other_bike_container)
+		other_bike_container.available_bikes
+	end
+
+
 		
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,6 +1,8 @@
 require './lib/van'
 
 van = Van.new
+station = DockingStation.new
+bike = Bike.new
 
 describe van do
 
@@ -9,6 +11,10 @@ describe van do
 		expect(van).to be_empty
 	end
 
+ it 'should be able to check for available bikes' do
+
+		expect(van.ask_for_bikes(station)).to eq []
+	end
 
 	
 end
